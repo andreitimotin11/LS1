53b54aa9293425dcfabea7d6d229ed783d0b8282
Andrei Timotin <andreitimotin11@gmail.com>
2017-07-24 20:55:37 +0300

    testing

---
+++ www/index.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Andrei
+ * Date: 24.07.2017
+ * Time: 20:41
+ */
+?>
+<html>
+<head>
+	<title> Test</title>
+</head>
+<body>
+
+<?php
+
+	echo 2+2;
+
+?>
+
+</body>
+</html>
